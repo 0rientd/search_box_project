b25fccdef143732e95a34bf1480d539cc2a8d5e1
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-07-24 15:47:18 -0300

    added stimulus-auto-submit

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
