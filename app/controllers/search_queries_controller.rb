8276323fc29f92fd54d95bf39b77fee617ff5f25
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-07-24 23:35:53 -0300

    added turbo stream to display the latest article found

--- app/controllers/search_queries_controller.rb
+++ app/controllers/search_queries_controller.rb
@@ -4,11 +4,18 @@ class SearchQueriesController < ApplicationController
   def create
     @query = SearchQuery.new(search_query_params)
     @query.ip_address = request.remote_ip
+    article = Article.find_by(title: @query.query)
 
-    if @query.save
-      redirect_to search_queries_path, notice: 'Registered search query successfully.'
-    else
-      render :index, alert: 'Error creating search query.'
+    if article
+      respond_to do |format|
+        if @query.save
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("last_queries", partial: "search_queries/partials/query_results", locals: { queries: SearchQuery.last(10).reverse }) }
+          format.html { redirect_to root_path, notice: 'Registered search query successfully.' }
+        else
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("last_queries", partial: "search_queries/partials/query_results", locals: { queries: SearchQuery.last(10).reverse }) }
+          format.html { redirect_to root_path, alert: 'Error creating search query.' }
+        end
+      end
     end
   end
 
